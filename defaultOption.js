a305d4b6849fa97721872ffe8a67d5645de3553e
Jiejw <771180118@qq.com>
2020-06-07 21:07:43 +0800

    优化配置箱

--- defaultOption.js
+++ defaultOption.js
@@ -1,95 +1,117 @@
-import { linearColors } from './colors';
-
+import { linearColors } from "./colors";
+import echarts from "echarts";
+require("echarts/map/js/china.js");
 const lineSeries = {
   showSymbol: false,
   lineStyle: {
-    type: 'solid',
+    type: "solid",
     shadowOffsetX: 0,
     shadowOffsetY: 2,
-    shadowBlur: 6
+    shadowBlur: 6,
   },
   label: {
     show: false,
-    position: 'top',
-    color: '#333',
-    formatter: '{c}'
-  }
+    position: "top",
+    color: "#333",
+    formatter: "{c}",
+  },
 };
+
 const pieSeries = {
   selectedOffset: 5,
   hoverOffset: 5,
-  radius: ['55%', '85%'],
+  radius: ["55%", "85%"],
   hoverAnimation: false,
   label: {
     show: false,
-    position: 'outside',
-    formatter: '{b}:{c}',
-    margin: '20%'
-  }
+    position: "outside",
+    formatter: "{b}:{c}",
+    margin: "20%",
+  },
 };
 
 const barSeries = {
   label: {
     show: false,
-    position: 'top',
-    formatter: '{c}',
-    color: '#333'
+    position: "top",
+    formatter: "{c}",
+    color: "#333",
   },
   barWidth: 10,
+  barGap: "0%",
   itemStyle: {
-    barBorderRadius: 5
-  }
+    barBorderRadius: 0,
+  },
 };
 
 const gaugeSeries = {
-  center: ['50%', '70%'],
-  type: 'gauge',
-  name: '项目名称',
-  radius: '100%',
-  splitNumber: 0,
-  startAngle: 180,
-  endAngle: 0,
+  center: ["50%", "50%"],
+  type: "gauge",
+  name: "项目名称",
+  radius: "96%",
+  splitNumber: 4,
+  startAngle: 225,
+  endAngle: -45,
+  min: 0,
+  max: 100,
+  // 仪表盘之间的分割线
   splitLine: {
-    show: false
+    show: false,
+    length: 7,
+    lineStyle: {
+      color: "auto",
+      width: 1,
+    },
   },
-  axisLabel: { show: false },
-  axisTick: { show: false },
-  pointer: { show: false },
+  // 仪表盘之间的刻度
+  axisTick: {
+    show: false,
+    length: 0,
+    lineStyle: {
+      color: "auto",
+    },
+  },
+  axisLabel: {
+    show: false,
+    color: "#454a55",
+    distance: 2,
+    fontSize: 12,
+    fontFamily: "PingFangSC-Regular",
+  },
+
+  pointer: { show: false, length: 20, width: 2 },
+
   axisLine: {
     lineStyle: {
-      width: 6,
-      shadowBlur: 0,
-      color: [
-        [0.5, '#2772FF'],
-        [1, '#F2F2F2']
-      ]
-    }
+      width: 3,
+      color: [[1, "#454A55"]],
+    },
   },
   detail: {
     show: false,
-    offsetCenter: [0, '-10%'],
+    offsetCenter: [0, "-10%"],
     rich: {
       name: {
         fontSize: 12,
         lineHeight: 12,
-        color: '#2B2E33'
+        color: "#2B2E33",
       },
       value: {
         fontSize: 16,
         lineHeight: 30,
-        color: '#14CC81'
-      }
-    }
-  }
+        color: "#14CC81",
+      },
+    },
+  },
 };
 
 // 合并对象,obj1是默认对象， obj2是传入对象
 function objCombine(...obj) {
-  const bool = obj.some(item => Array.isArray(item));
+  const bool = obj.some((item) => Array.isArray(item));
   const res = bool ? [] : {};
 
   function combine(key, item, res) {
-    const isObj = typeof item[key] === 'object';
+    const isObj = typeof item[key] === "object";
     if (isObj) {
       if (!res[key]) {
         res[key] = Array.isArray(res[key]) ? [] : {};
@@ -98,7 +120,7 @@ function objCombine(...obj) {
       if (Array.isArray(item[key])) {
         res[key] = item[key];
       } else {
-        Object.keys(item[key]).forEach(_item => {
+        Object.keys(item[key]).forEach((_item) => {
           combine(_item, item[key], res[key]);
         });
       }
@@ -106,8 +128,8 @@ function objCombine(...obj) {
       res[key] = item[key];
     }
   }
-  obj.forEach(item => {
-    Object.keys(item).forEach(key => {
+  obj.forEach((item) => {
+    Object.keys(item).forEach((key) => {
       combine(key, item, res);
     });
   });
@@ -115,15 +137,15 @@ function objCombine(...obj) {
   return res;
 }
 
-const getSeries = item => {
-  switch (item.type || 'line') {
-    case 'line':
+const getSeries = (item) => {
+  switch (item.type || "line") {
+    case "line":
       return objCombine(lineSeries, item);
-    case 'bar':
+    case "bar":
       return objCombine(barSeries, item);
-    case 'pie':
+    case "pie":
       return objCombine(pieSeries, item);
-    case 'gauge':
+    case "gauge":
       return objCombine(gaugeSeries, item);
     default:
       return item;
@@ -131,169 +153,188 @@ const getSeries = item => {
 };
 
 const getOption = (customize, type = false) => {
-  const { grid, tooltip, yAxis, xAxis, series = [], ...rest } = customize;
+  const {
+    grid,
+    tooltip,
+    yAxis,
+    xAxis,
+    series = [],
+    legend,
+    geo,
+    ...rest
+  } = customize;
   // 判断serise内部是否使用的类目轴
-  const seriesType = series.map(item => item.type);
-  const bool = seriesType.some(item => ['bar', 'line'].includes(item));
-  const defaultxAxis = [
-    {
-      type: 'category',
-      nameLocation: 'end',
-      nameTextStyle: {
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 18
-      },
-      axisLine: {
-        show: false
-      },
-      axisTick: {
-        show: false,
-        alignWithLabel: true,
-        interval: 0,
-        inside: false
-      },
-      splitLine: {
-        show: false,
-        interval: 0,
-        lineStyle: {
-          color: '#ccc',
-          width: 0.5,
-          type: 'dashed',
-          opacity: 0.5
-        }
-      },
-      axisLabel: {
-        show: true,
-        interval: 0,
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 12
-      },
-      axisPointer: {
-        show: true,
-        type: 'line',
-        label: {
-          show: false,
-          margin: 5,
-          color: '#999',
-          fontFamily: 'PingFangSC-Regular, sans-serif',
-          backgroundColor: 'none',
-          fontSize: 8
-        },
-        lineStyle: {
-          color: '#979797',
-          width: 0.8
-        },
-        shadowStyle: {
-          color: '#5594F2'
-        },
-        triggerTooltip: true
-      }
-    }
-  ];
-  const defaultyAxis = [
-    {
-      show: true,
-      position: 'left',
-      nameTextStyle: {
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 18
-      },
-      axisLine: {
-        show: false
-      },
-      axisTick: {
-        show: false,
-        alignWithLabel: true,
-        interval: 0,
-        inside: false
-      },
-      axisLabel: {
-        show: true,
-        interval: 0,
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 12
-      },
-      splitLine: {
-        show: false,
-        interval: 0,
-        lineStyle: {
-          color: '#ccc',
-          width: 0.5,
-          type: 'dashed',
-          opacity: 0.5
-        }
-      }
+  const seriesType = series.map((item) => item.type);
+  const bool = seriesType.some((item) => ["bar", "line"].includes(item));
+
+  const defaultLegend = {
+    show: false,
+    bottom: 0,
+    itemGap: 60,
+    itemWidth: 20,
+    itemHeight: 20,
+    borderRadius: 0,
+    icon: "rect",
+    textStyle: {
+      color: "#666",
+      borderRadius: 0,
     },
-    {
+    data: [],
+  };
+
+  const defaultxAxis = {
+    type: "category",
+    nameLocation: "end",
+    nameTextStyle: {
+      fontFamily: "PingFangSC-Regular, sans-serif",
+      fontSize: 18,
+    },
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+      alignWithLabel: true,
+      interval: 0,
+      inside: false,
+    },
+    splitLine: {
+      show: false,
+      interval: 0,
+      lineStyle: {
+        color: "#ccc",
+        width: 0.5,
+        type: "dashed",
+        opacity: 0.5,
+      },
+    },
+    axisLabel: {
       show: true,
-      position: 'left',
-      nameTextStyle: {
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 18
-      },
-      axisLine: {
-        show: false
-      },
-      axisTick: {
+      interval: 0,
+      fontFamily: "PingFangSC-Regular, sans-serif",
+      fontSize: 12,
+    },
+    axisPointer: {
+      show: true,
+      type: "line",
+      label: {
         show: false,
-        alignWithLabel: true,
-        interval: 0,
-        inside: false
+        margin: 5,
+        color: "#999",
+        fontFamily: "PingFangSC-Regular, sans-serif",
+        backgroundColor: "none",
+        fontSize: 8,
       },
-      axisLabel: {
-        show: true,
-        interval: 0,
-        fontFamily: 'PingFangSC-Regular, sans-serif',
-        fontSize: 12
+      lineStyle: {
+        color: "#979797",
+        width: 0.8,
       },
-      splitLine: {
-        show: false,
-        interval: 0,
-        lineStyle: {
-          color: '#ccc',
-          width: 0.5,
-          type: 'dashed',
-          opacity: 0.5
-        }
-      }
-    }
-  ];
+      shadowStyle: {
+        color: "#5594F2",
+      },
+      triggerTooltip: true,
+    },
+  };
+
+  const defaultyAxis = {
+    show: true,
+    position: "left",
+    nameTextStyle: {
+      fontFamily: "PingFangSC-Regular, sans-serif",
+      fontSize: 18,
+    },
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+      alignWithLabel: true,
+      interval: 0,
+      inside: false,
+    },
+    axisLabel: {
+      show: true,
+      interval: 0,
+      fontFamily: "PingFangSC-Regular, sans-serif",
+      fontSize: 12,
+    },
+    splitLine: {
+      show: false,
+      interval: 0,
+      lineStyle: {
+        color: "#ccc",
+        width: 0.5,
+        type: "dashed",
+        opacity: 0.5,
+      },
+    },
+  };
+
   const defaultTooltip = {
     show: false,
-    trigger: bool ? 'axis' : 'item',
-    triggerOn: 'mousemove|click',
+    trigger: bool ? "axis" : "item",
+    triggerOn: "mousemove|click",
     confine: true,
-    formatter: '{b}: {c}',
-    backgroundColor: 'rgba(1,27,100,0.7)',
+    formatter: "{b}: {c}",
+    backgroundColor: "rgba(1,27,100,0.7)",
     textStyle: {
-      fontSize: 10
-    }
+      fontSize: 10,
+    },
   };
-  const tempSeries = series.map(item => getSeries(item));
 
-  let tempxAxis = [];
-  let tempyAxis = [];
-  let tempTooltip = {};
+  const defaultGeo = {
+    map: "china",
+    zoom: 1.25,
+    roam: false,
+    itemStyle: {
+      borderColor: "#232532",
+      borderWidth: 1.5,
+      areaColor: "#8d9bc8",
+      emphasis: {
+        borderWidth: 1.6,
+        areaColor: "#8d9bc8",
+        borderColor: "#232532",
+      },
+    },
+  };
 
-  tempxAxis = xAxis ? objCombine(defaultxAxis, xAxis) : defaultxAxis;
-  tempyAxis = yAxis ? objCombine(defaultyAxis, yAxis) : defaultyAxis;
+  const tempSeries = series.map((item) => getSeries(item));
+
+  let tempxAxis, tempLegend, tempyAxis, tempTooltip, tempGeo;
+
+  tempxAxis =
+    xAxis instanceof Array
+      ? xAxis.map((item) => {
+          return objCombine(defaultxAxis, item);
+        })
+      : objCombine(defaultxAxis, xAxis || {});
+  tempyAxis =
+    yAxis instanceof Array
+      ? yAxis.map((item) => {
+          return objCombine(defaultyAxis, item);
+        })
+      : objCombine(defaultyAxis, yAxis || {});
+  tempGeo = geo ? objCombine(defaultGeo, geo) : null;
+
   tempTooltip = tooltip ? objCombine(defaultTooltip, tooltip) : defaultTooltip;
+  tempLegend = legend ? objCombine(defaultLegend, legend) : defaultLegend;
   return {
     color: type ? linearColors() : null,
     tooltip: tempTooltip,
     grid: {
       containLabel: true,
-      top: 10,
+      top: 0,
       left: 0,
-      bottom: 0,
+      bottom: tempLegend.show ? 30 : 0,
       right: 0,
-      ...grid
+      ...grid,
     },
+    geo: tempGeo,
+    legend: tempLegend,
     xAxis: tempxAxis,
     yAxis: tempyAxis,
     series: tempSeries,
-    // series,
-    ...rest
+    ...rest,
   };
 };
 
